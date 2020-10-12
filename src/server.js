aa4c3802e7b44c8a805f3111498f935953c8f83a
Julien Tromas <julien.tromas@gmail.com>
2020-10-12 17:43:32 +0900

    Refactored the GET /api/pokemon request code and it passes the tests
    Co-authored-by: Yoshi106 <yoshi106@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -7,22 +7,39 @@ const setupServer = () => {
   server.use(express.json());
 
   server.get("/api/pokemon", (req, res) => {
-    if (!req.query) res.send(pokeData.pokemon);
-    res.send(pokeData.pokemon);
-  });
-  server.get("/api/pokemon/:limit", (req, res) => {
-    const pokemonLimit = req.params.limit;
+    if (!req.query.limit) res.send(pokeData.pokemon);
+    const { limit } = req.query;
     const output = [];
-    for (let i = 0; i < Number(pokemonLimit); i++) {
+    for (let i = 0; i < Number(limit); i++) {
       output.push(pokeData.pokemon[i]);
     }
     res.send(output);
   });
-
   server.post("/api/pokemon", (req, res) => {
     const newPokemon = req.body;
     pokeData.pokemon.push(newPokemon);
     res.send(newPokemon);
+  });
+
+  server.get("/api/pokemon/:id", (req, res) => {
+    const pokemonId = req.params.id;
+    res.send(pokeData.pokemon[Number(pokemonId - 1)]);
+  });
+
+  server.get("/api/pokemon/:name", (req, res) => {
+    const pokemonName = req.params.name;
+    const output = pokeData.pokemon.filter(
+      (pokemon) => pokemon.name === pokemonName
+    )[0];
+    res.send(output);
+  });
+
+  server.patch("/api/pokemon/name/:idOrname", (req, res) => {
+    const pokemonName = req.params.name;
+    const output = pokeData.pokemon.filter(
+      (pokemon) => pokemon.name === pokemonName
+    )[0];
+    res.send(output);
   });
   return server;
 };
