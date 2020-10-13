5850e600536ef213ab451f285fa55522b90afa13
Julien Tromas <julien.tromas@gmail.com>
2020-10-13 12:29:06 +0900

    Wrote requests from Delete/api/pokemon to Get api/attacks  and the tests who go with them
    Co-authored-by: Yoshi106 <yoshi106@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const pokeData = require("./data");
 const express = require("express");
+const { pokemon } = require("./data");
 
 const setupServer = () => {
+  function objectSlicer(object, lastKey) {
+    const filteredKeys = Object.keys(object).slice(0, lastKey);
+    const newObject = {};
+    filteredKeys.forEach(function(key) {
+      newObject[key] = object[key];
+    });
+    return newObject;
+  }
+
   const server = express();
 
   server.use(express.json());
@@ -49,14 +59,100 @@ const setupServer = () => {
     res.send(output);
   });
 
-  /*
+  server.delete("/api/pokemon/:idOrName", (req, res) => {
+    if (Number(req.params.idOrName)) {
+      const pokemonId = req.params.idOrName;
+      const output = pokeData.pokemon.splice(pokemonId - 1, 1);
+      res.send(output[0]);
+    } else {
+      const pokemonName = req.params.idOrName;
+      const removedPokemonId = pokeData.pokemon.filter(
+        (pokemon) => pokemon.name === pokemonName
+      )[0].id;
+      const output = pokeData.pokemon.splice(removedPokemonId - 1, 1);
+      res.send(output[0]);
+    }
+  });
 
-  DELETE : get and ID or a NAME of a pokemon, find it in the array with its id,
-  then remove it from the array with splice
+  server.get("/api/pokemon/:idOrName/evolutions", (req, res) => {
+    if (Number(req.params.idOrName)) {
+      const pokemonId = req.params.idOrName;
+      const output = pokeData.pokemon[pokemonId - 1];
+      if (output.evolutions) res.send(output.evolutions);
+      else {
+        res.send([]);
+      }
+    } else {
+      const pokemonName = req.params.idOrName;
+      const output = pokeData.pokemon.filter(
+        (pokemon) => pokemon.name === pokemonName
+      )[0];
+      if (output.evolutions) res.send(output.evolutions);
+      else {
+        res.send([]);
+      }
+    }
+  });
 
+  server.get("/api/pokemon/:idOrName/evolutions/previous", (req, res) => {
+    if (Number(req.params.idOrName)) {
+      const pokemonId = req.params.idOrName;
+      const output = pokeData.pokemon[pokemonId - 1];
+      res.send(output["Previous evolution(s)"]);
+    } else {
+      const pokemonName = req.params.idOrName;
+      const output = pokeData.pokemon.filter(
+        (pokemon) => pokemon.name === pokemonName
+      )[0];
+      res.send(output["Previous evolution(s)"]);
+    }
+  });
 
+  server.get("/api/types", (req, res) => {
+    if (!req.query.limit) res.send(pokeData.types);
+    const { limit } = req.query;
+    const output = [];
+    for (let i = 0; i < Number(limit); i++) {
+      output.push(pokeData.types[i]);
+    }
+    res.send(output);
+  });
 
-  */
+  server.post("/api/types", (req, res) => {
+    const newType = req.body;
+    pokeData.types.push(newType);
+    res.send(newType);
+  });
+
+  server.delete("/api/types/:name", (req, res) => {
+    const typeName = req.params.name;
+    const removedType = pokeData.types.filter((type) => type === typeName)[0];
+    const output = pokeData.types.splice(
+      pokeData.types.indexOf(removedType),
+      1
+    );
+    res.send(output[0]);
+  });
+
+  server.get("/api/types/:type/pokemon", (req, res) => {
+    const typeName = req.params.type;
+    const output = pokeData.pokemon.filter((pokemon) =>
+      pokemon.types.includes(typeName)
+    );
+    const pokemonList = [];
+    for (let i = 0; i <= output.length - 1; i++) {
+      pokemonList.push(objectSlicer(output[i], 2));
+    }
+    res.send(pokemonList);
+  });
+  server.get("/api/attacks", (req, res) => {
+    if (!req.query.limit) res.send(pokeData.attacks);
+    const { limit } = req.query;
+    const output = [];
+    output.push(pokeData.attacks);
+    res.send(output);
+  });
+
   return server;
 };
 
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -1,5 +1,6 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const { pokemon } = require("../src/data");
 chai.use(chaiHttp);
 chai.should();
 const pokeData = require("../src/data");
@@ -118,12 +119,161 @@ describe("Pokemon API Server", () => {
     });
   });
   describe("PATCH /api/pokemon/:idOrName", () => {
-    it("should be able to modify the pokemon with the corresponding id", async () => {
+    it("should be able to modify the pokemon with the corresponding id or name", async () => {
       const res = await request
         .patch("/api/pokemon/Butterfree")
         .query({ name: "ButterTakai" });
       res.should.be.json;
       JSON.parse(res.text).name.should.deep.equal("ButterTakai");
+    });
+  });
+  describe("DELETE /api/pokemon/:idOrName", () => {
+    it("should be able to delete the pokemon with the corresponding id or name", async () => {
+      const removedPokemon = {
+        id: "102",
+        name: "Exeggcute",
+        classification: "Egg Pokémon",
+        types: ["Grass", "Psychic"],
+        resistant: [
+          "Water",
+          "Electric",
+          "Grass",
+          "Fighting",
+          "Ground",
+          "Psychic",
+        ],
+        weaknesses: ["Fire", "Ice", "Poison", "Flying", "Bug", "Ghost", "Dark"],
+        weight: {
+          minimum: "2.19kg",
+          maximum: "2.81kg",
+        },
+        height: {
+          minimum: "0.35m",
+          maximum: "0.45m",
+        },
+        fleeRate: 0.1,
+        evolutionRequirements: {
+          amount: 50,
+          name: "E",
+        },
+        evolutions: [
+          {
+            id: 103,
+            name: "Exeggutor",
+          },
+        ],
+        maxCP: 978,
+        maxHP: 1099,
+        attacks: {
+          fast: [
+            {
+              name: "Confusion",
+              type: "Psychic",
+              damage: 15,
+            },
+          ],
+          special: [
+            {
+              name: "Ancient Power",
+              type: "Rock",
+              damage: 35,
+            },
+            {
+              name: "Psychic",
+              type: "Psychic",
+              damage: 55,
+            },
+            {
+              name: "Seed Bomb",
+              type: "Grass",
+              damage: 40,
+            },
+          ],
+        },
+      };
+      const res = await request.delete("/api/pokemon/102");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(removedPokemon);
+    });
+  });
+  describe("GET /api/pokemon/:idOrName/evolutions", () => {
+    it("should return the evolutions a Pokemon has", async () => {
+      const res = await request.get("/api/pokemon/Staryu/evolutions");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal([{ id: 121, name: "Starmie" }]);
+    });
+    it("should return an empty array if the Pokemon doesn't have any evolution", async () => {
+      const res = await request.get("/api/pokemon/Starmie/evolutions");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal([]);
+    });
+  });
+  describe("GET /api/pokemon/:idOrName/evolutions/previous", () => {
+    it("should return the previous evolutions a Pokemon has", async () => {
+      const res = await request.get("/api/pokemon/Starmie/evolutions/previous");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal([{ id: 120, name: "Staryu" }]);
+    });
+  });
+  describe("GET /api/types", () => {
+    it("should return the list of all the pokemon types", async () => {
+      const res = await request.get("/api/types");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokeData.types);
+    });
+    it("should return a limited list of pokemon types", async () => {
+      const res = await request.get("/api/types").query({ limit: "10" });
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokeData.types.slice(0, 10));
+    });
+  });
+  describe("POST /api/types", () => {
+    it("should add a pokemon type", async () => {
+      const newType = "Pizza";
+      const res = await request.post("/api/types").send(newType);
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokeData.types[17]);
+    });
+  });
+  describe("DELETE /api/types/:name", () => {
+    it("should remove a pokemon type", async () => {
+      const removedType = "Electric";
+      const res = await request.delete("/api/types/Electric");
+      res.should.be.html;
+      res.text.should.deep.equal(removedType);
+    });
+  });
+  describe("GET /api/types/:type/pokemon", () => {
+    it("should return the list of all the pokemon of the given type", async () => {
+      const fireTypePokemon = [
+        { id: "004", name: "Charmander" },
+        { id: "005", name: "Charmeleon" },
+        { id: "006", name: "Charizard" },
+        { id: "037", name: "Vulpix" },
+        { id: "038", name: "Ninetales" },
+        { id: "058", name: "Growlithe" },
+        { id: "059", name: "Arcanine" },
+        { id: "077", name: "Ponyta" },
+        { id: "078", name: "Rapidash" },
+        { id: "126", name: "Magmar" },
+        { id: "136", name: "Flareon" },
+        { id: "146", name: "Moltres" },
+      ];
+      const res = await request.get("/api/types/Fire/pokemon");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(fireTypePokemon);
+    });
+  });
+  describe("GET /api/attacks", () => {
+    it("should return the list of all the pokemon attacks", async () => {
+      const res = await request.get("/api/attacks");
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokeData.attacks);
+    });
+    it("should return a limited list of pokemon attacks", async () => {
+      const res = await request.get("/api/attacks").query({ limit: "1" });
+      res.should.be.json;
+      JSON.parse(res.text).should.deep.equal(pokeData.attacks.fast);
     });
   });
 });
